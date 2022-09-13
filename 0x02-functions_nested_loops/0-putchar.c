77c86aba61950c9703edfa8bc4916ff123b3d90c
Peter <kgasane.pm123@gmail.com>
2022-09-13 09:24:25 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,22 @@
-#include <stdio.h>
-#include <main.h>
+#include "main.h"
 
 /**
- * main - entry pont
+ * main - entrypoint
  *
  * Return - always 0 (success)
  *
  */
 int main(void)
 {
-	_putchar("_puthar\n", c);
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 	return (0);
+	
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char c);
+/**
+ * function prints out character c to standard out put
+ */
+
+#endif
