e94de8e55f6f346c8181da2fa7da4a0795b4a05b
Peter <kgasane.pm123@gmail.com>
2022-09-15 06:43:27 -0700

    print the last digit of the number stored in the variable n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*more headers goes there */
-
-/*betty style doc for function main goes there */
+/**
+*more headers goes there
+*main - entry point
+Return: always 0
+*
+*betty style doc for function main goes there
+*
+*/
 int main(void)
 {
 	int n;
