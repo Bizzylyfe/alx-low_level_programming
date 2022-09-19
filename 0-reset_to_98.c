698f2e52f0334b82c7a4d0ee67c6d8741dfa3efc
Peter <kgasane.pm123@gmail.com>
2022-09-19 06:07:10 -0700

    reset given value to 98

---
+++ 0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - entry pont
+ * @n : parameter
+ * reset_to_98 - reset the value of given integer to 98
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
